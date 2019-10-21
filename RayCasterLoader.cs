80008ea50f8ec82e9623be145151d6b072d72292
Khaled72 <38741989+Khaled72@users.noreply.github.com>
2019-10-22 00:06:51 +0200

    Add files via upload

---
+++ RayCasterLoader.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class RayCasterLoader : MonoBehaviour
+{
+
+    public float maxTimeToTeriggre;
+    public Image imageFiller;
+    RaycastHit hit;
+    float Timer=0;
+    GameObject oldTarget;
+    void Start()
+    {
+        Timer = 0;
+    }
+
+    void Update()
+    {
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+        {
+            if (hit.collider.tag == "Interactable")
+            {
+                if (hit.collider.gameObject == oldTarget)
+                {
+                    if (Timer >= maxTimeToTeriggre)
+                    {
+                        Timer = 0;
+                        hit.collider.GetComponent<InteractableEvents>().events.Invoke();
+                        Debug.Log("Doing Someting");
+                        imageFiller.fillAmount = Timer / maxTimeToTeriggre;
+                    }
+                    else
+                    {
+                        Timer += Time.deltaTime;
+                        //Fill Amount of Image
+                        imageFiller.fillAmount = Timer / maxTimeToTeriggre;
+                    }
+                }
+                else
+                {
+                    Timer = 0;
+                    oldTarget = hit.collider.gameObject;
+                    Timer += Time.deltaTime;
+                    imageFiller.fillAmount = Timer / maxTimeToTeriggre;
+                }
+            }
+            else
+            {
+                Timer = 0;
+                oldTarget = null;
+                imageFiller.fillAmount = Timer / maxTimeToTeriggre;
+            }
+        }
+        else
+        {
+            Timer = 0;
+            oldTarget = null;
+            imageFiller.fillAmount = Timer / maxTimeToTeriggre;
+        }
+    }
+}
---
+++ WrongAnswer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class WrongAnswer : MonoBehaviour
+{
+    public Color normalColor;
+    public Color wrongColor;
+    Image image;
+    // Start is called before the first frame update
+    void Start()
+    {
+        image = GetComponent<Image>();
+        image.color = normalColor;
+    }
+    public void LerpColor() 
+    {
+       StartCoroutine(LerpColorToWrongAndBackTONormal(0.2f));
+    }
+    IEnumerator LerpColorToWrongAndBackTONormal(float aTime)
+    {
+        Color color = image.color;
+        for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / aTime)
+        {
+            image.color = Color.Lerp(color,wrongColor,t);
+            yield return null;
+        }
+        yield return new WaitForSeconds(1.5f);
+        color = image.color;
+        for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / aTime)
+        {
+            image.color = Color.Lerp(color, normalColor, t);
+            yield return null;
+        }
+    }
+}
